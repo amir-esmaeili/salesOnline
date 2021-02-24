7b59d048d5ba55bb18331c9447126e28933b9cad
Amir Esmaeili <amiresm17@gmail.com>
2021-02-24 19:36:05 +0330

    paging added for sellers product

--- controllers/products.go
+++ controllers/products.go
@@ -1,11 +1,12 @@
 package controllers
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 	"personal/forms"
 	"personal/models"
+	"strconv"
 )
 
 func NewProduct(context *gin.Context) {
@@ -34,8 +35,19 @@ func NewProduct(context *gin.Context) {
 
 func GetSellerProducts(context *gin.Context)  {
 	sellerId := context.Param("seller_id")
+	pageParam := context.DefaultQuery("page", "0")
+	page, err := strconv.Atoi(pageParam)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Page number is not valid",
+		})
+	}
 	var products []models.Product
-	err := models.DB.Where("seller_id = ?", sellerId).Find(&products)
-	fmt.Println(err)
+	models.DB.Where("seller_id = ?", sellerId).Find(&products)
+	if page != 0 {
+		postPerPage, _ := strconv.Atoi(os.Getenv("POST_PER_PAGE"))
+		context.JSON(http.StatusOK, products[postPerPage* (page - 1): postPerPage* page])
+		return
+	}
 	context.JSON(http.StatusOK, products)
 }
